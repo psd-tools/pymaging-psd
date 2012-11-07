6bd9379de15c9b5b3ed6fd9566e520eaaf25c28f
Mikhail Korobov <kmike84@gmail.com>
2012-11-08 01:31:47 +0600

    NotImplementedError -> FormatNotSupported

--- pymaging_psd.py
+++ pymaging_psd.py
@@ -3,12 +3,13 @@ from __future__ import absolute_import
 
 import psd_tools
 from pymaging.formats import Format
+from pymaging.exceptions import FormatNotSupported
 
 def decode(fileobj):
     psd = psd_tools.PSDImage.from_stream(fileobj)
     return psd.as_pymaging()
 
 def encode(image, fileobj):
-    raise NotImplementedError("Saving to PSD is not supported")
+    raise FormatNotSupported('psd')
 
 PSD = Format(decode, encode, ['psd'])
